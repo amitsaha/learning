3e53619a699de146c69e35e57c9853aa787029e9
Amit Saha <amitsaha.in@gmail.com>
2014-11-18 08:03:39 +1000

    hopefully correct cycle detection

--- Python/graphs/d_graph.py
+++ Python/graphs/d_graph.py
@@ -1,18 +1,8 @@
 '''
 Implement a directed graph and perform basic graph operations
-
-Example graph:
-
-        A -> B -> D
-        |    |
-       \ /  \/
-   F <- C    E
-        |    |
-       \/   \/
-       G     H
 '''
 from __future__ import print_function
-from collections import deque
+from collections import deque, OrderedDict
 
 class Node:
     def __init__(self, label, neighbors=[]):
@@ -20,7 +10,10 @@ class Node:
         self.neighbors = neighbors
 
     def __str__(self):
-        return self.label
+        return self.label + '->' + str([n.label for n in self.neighbors])
+
+    def update_neighbors(self, neighbors=[]):
+        self.neighbors = neighbors
 
 def visit(node):
     print(node)
@@ -37,17 +30,38 @@ def bfs(origin):
         visit(cur)
 
 # depth first search
-def dfs(origin):
+def dfs(origin, visitor_func):
     # visit the origin
-    visit(origin)
+    visitor_func(origin)
     # visit the neighbors
     to_visit = origin.neighbors
     while to_visit:
         cur = to_visit.pop()
         to_visit.extend(cur.neighbors)
-        visit(cur)
+        visitor_func(cur)
+
+# detect if there is a cycle in the graph
+visited = OrderedDict()
+def visit_with_cycle_detection(node):
+    print(node)
+    if not visited.get(node, None):
+        visited[node] = True
+    else:
+        print('Cycle detected: {0}'.format(str([n.label for n in visited.keys()])))
+        raise SystemExit
 
 if __name__ == '__main__':
+    '''
+    Example graph:
+
+        A -> B -> D
+        |    |
+       \ /  \/
+   F <- C    E
+        |    |
+       \/   \/
+       G     H
+    '''
     d = Node('D')
     f = Node('F')
     g = Node('G')
@@ -59,4 +73,18 @@ if __name__ == '__main__':
     a = Node('A', neighbors=[b, c])
 
     bfs(a)
-    dfs(a)
+    dfs(a, visit)
+
+    # Graph with a cycle
+    '''
+          A -> C -> B
+          |<------- |
+    '''
+
+    a = Node('A')
+    b = Node('B')
+    c = Node('C')
+    a.update_neighbors([c])
+    c.update_neighbors([b])
+    b.update_neighbors([a])
+    dfs(a, visit_with_cycle_detection)   
