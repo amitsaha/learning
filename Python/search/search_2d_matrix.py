ef9ec8c530a6612a9e2ed458d7502a55930d4e11
Amit Saha <amitsaha.in@gmail.com>
2014-11-23 22:18:14 +1000

    mods

--- Python/search/search_2d_matrix.py
+++ Python/search/search_2d_matrix.py
@@ -1,17 +1,22 @@
 '''
-Search for an element in a 2-D sorted matrix:
+Search for an element in a 2-D  matrix
+'''
+
+from __future__ import print_function
+'''
+Sorted matrix:
 
 1 3 5
 7 9 11
 13 15 17
+
 '''
-from __future__ import print_function
-def find_elem_matrix(mat, item):
+
+def find_elem_sorted_matrix(mat, item):
     start = 0
     end = len(mat[0]) * len(mat) - 1
-
     while start <= end:
-        mid = int((start+end)/2)
+        mid = int((start + end)/2)
         r = int(mid % len(mat[0]))
         c = int(mid / len(mat[0]))
 
@@ -25,10 +30,47 @@ def find_elem_matrix(mat, item):
 
     return False
 
-m = [[1, 3, 5],
+m = [[1, 3, 5], 
      [7, 9, 11],
      [13, 15, 17]]
-print(find_elem_matrix(m, 7))
-print(find_elem_matrix(m, 9))
-print(find_elem_matrix(m, 17))
-print(find_elem_matrix(m, 90))
+
+print('Search in fully sorted matrix:')
+
+print(find_elem_sorted_matrix(m, 7))
+print(find_elem_sorted_matrix(m, 9))
+print(find_elem_sorted_matrix(m, 18))
+
+
+'''
+Row sorted and column sorted:
+
+1 2 8 9
+2 4 9 12
+4 7 10 13
+6 8 11 15
+'''
+def find_elem_partially_sorted(mat, item):
+    for row in range(len(mat)):
+        start = 0
+        end = len(mat[0]) - 1
+
+        while start <= end:
+            mid = int((start + end)/2)
+            if mat[row][mid] == item:
+                return row, mid
+            else:
+                if mat[row][mid] > item:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+    return False
+
+m = [[1, 2, 8, 9],
+     [2, 4, 9, 12],
+     [4, 7, 10, 13],
+     [6, 8, 11, 15],]
+
+print('Search in partially sorted matrix:')
+print(find_elem_partially_sorted(m, 7))
+print(find_elem_partially_sorted(m, 11))
+print(find_elem_partially_sorted(m, 16))
--- Python/sorting/counting_sort.py
+++ Python/sorting/counting_sort.py
@@ -7,16 +7,15 @@ http://en.wikipedia.org/wiki/Counting_sort
 def sort_ages(arr):
 
     max_age = 99
-    age_count = {}
+    age_count = [0] * 100
     # O(n) time complexity
-    # storage space for the dictionary: 100 max.
+    # storage space for the dictionary: 100 
     for i in arr:
-        age_count[i] = age_count.get(i, 0) + 1
+        age_count[i] += 1
     # Print them
-    for i in range(0, max_age):
-        if age_count.get(i, None):
-            for j in range(age_count[i]):
-                print(i)
+    for i in range(len(age_count)):
+        for j in range(age_count[i]):
+            print(i)
 
 # test
 sort_ages([56, 34, 50, 27, 28, 27])
