cd1505cb58030ae345cc7d65851569febab232c4
Amit Saha <amitsaha.in@gmail.com>
2014-12-17 07:48:09 +1000

    random selection with percentage

---
+++ recipes/percentage_random.py
@@ -0,0 +1,28 @@
+import random
+import matplotlib.pyplot as plt
+
+# return a random index based on the percentages
+def get_index(r, percentages):
+    c_percentage = 0
+    sum_percentages = []
+    for p in percentages:
+        c_percentage += p
+        sum_percentages.append(c_percentage)
+    for item, sp in enumerate(sum_percentages):
+        if r <= sp:
+            return item
+    # if we are here, it means we are somewhere between
+    # sum(percentages) and 1.0, so, we assume it falls into the
+    # last category
+    return len(percentages)-1
+
+indices = []
+percentages = [0.25, 0.25, 0.1, 0.4]
+for i in range(10000):
+    r = random.random()
+    idx = get_index(r, percentages)
+    indices.append(idx)
+
+# plot a histogram to make the random choices apparent
+plt.hist(indices, bins=len(percentages))
+plt.show()
