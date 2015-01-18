5c1dd9ceba1780749844f81b150e08c977a4bf93
Amit Saha <amitsaha.in@gmail.com>
2015-01-18 20:28:48 +1000

    find_missing: edits

--- Python/find_missing.py
+++ Python/find_missing.py
@@ -5,17 +5,23 @@ integers in 1..N
 
 def find_missing(arr):
     N = len(arr)
-    table = set(arr)
 
-    if N == 1:
-        if arr[0] != 1:
-            return 1
-        else:
-            0
+    # empty array
+    if N == 0:
+        return None
+
+    # create a set to allow average O(1) lookup
+    table = set(arr)
     for num in range(1, N+1):
         if num not in table:
             return num
+    return None
 
-assert find_missing([2]) == 1
-assert find_missing([5, 4, 1, 3]) == 2
-assert find_missing([6, 4, 1, 3, 5]) == 2
+# test cases
+assert find_missing([]) == None
+assert find_missing([1]) == None
+assert find_missing([3, 4, 2, 2]) == 1
+assert find_missing([3, 3, 1, 3]) == 2
+assert find_missing([4, 4, 1, 3, 5]) == 2
+assert find_missing([1, 1, 1, 1, 1]) == 2
+
