a1aa3f95a1e7ceef08b3d0347781bf66b2e78369
Amit Saha <amitsaha.in@gmail.com>
2015-02-02 00:01:53 +1000

    Collapse range

--- Python/collapse_range.py
+++ Python/collapse_range.py
@@ -1,23 +1,38 @@
 '''
+Given a number of integer intervals, collapse them:
+
 Input: [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
 Output:   [(0, 1), (3, 8), (9, 12)]
 '''
 
 def collapse_range(slots):
-    
-    hours = []
-    for s in slots:
-        hours.append(range(s[0], s[1]+1))
-    all_hours = list(set(hours[0]).union(*hours[1:]))
 
-    start = all_hours[0]
-    for pos, item in enumerate(all_hours):
-        if pos < len(all_hours)-2 and item + 1 == all_hours[pos+1]:
-            continue
-        else:
-            print start, item
-            start = all_hours[pos+1]
+    # order based on the first slot
+    ordered_slots = sorted(slots, key=lambda range: range[0])
+    merged_slots = []
+    i = 0
+    j = 0
+    while i < len(ordered_slots):
+        j = i+1
+        l, u = ordered_slots[i]
+        while j < len(ordered_slots):
+            if ordered_slots[i][1] >= ordered_slots[j][0]:
+                u = ordered_slots[j][1]
+                i += 1
+                j += 1
+                continue
+            else:
+                break
+        i = j
+        merged_slots.append((l, u))
 
-collapse_range([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
-    
-    
+    return merged_slots
+
+assert collapse_range([(0, 1), (3, 5), (4, 8), (10, 12), (12, 14), (9, 10)]) ==  \
+    [(0, 1), (3, 8), (9, 14)]
+assert collapse_range([(0, 1), (3, 5), (6, 8), (10, 12), (8, 10)]) ==  \
+    [(0, 1), (3, 5), (6, 12)]
+assert collapse_range([(0, 1), (3, 5)])  ==  \
+    [(0, 1), (3, 5)]
+assert collapse_range([(0, 2), (2, 5)])  ==  \
+    [(0, 5)]
