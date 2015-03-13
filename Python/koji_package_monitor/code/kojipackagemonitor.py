302105818b962abc5ed49b9964b3f70989ef3898
Amit Saha <amitsaha.in@gmail.com>
2015-03-13 05:33:51 +0000

    Koji package monitor

---
+++ Python/koji_package_monitor/code/kojipackagemonitor.py
@@ -0,0 +1,27 @@
+# An example fedmsg koji consumer
+
+import fedmsg.consumers
+
+class KojiPackageMonitor(fedmsg.consumers.FedmsgConsumer):
+    topic = 'org.fedoraproject.prod.buildsys.build.state.change'
+    # http://fedmsg2.readthedocs.org/en/latest/topics.html#buildsys
+    config_key = 'kojipackagemonitor'
+
+    def __init__(self, *args, **kw):
+        super(KojiPackageMonitor, self).__init__(*args, **kw)
+
+    def consume(self, msg):
+        new_state = msg['body']['msg']['new']
+        # care only about completed packages
+        if new_state == 3:
+            name = msg['body']['msg']['name']
+            version = msg['body']['msg']['version']
+            release = msg['body']['msg']['release']
+            instance = msg['body']['msg']['instance']
+
+            print name, version, release,  instance
+
+            
+
+        
+        
