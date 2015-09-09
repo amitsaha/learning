abc9d3b82f4c9fb77813aa9ca96b2894fb01f0fb
Amit Saha <amitsaha.in@gmail.com>
2015-09-09 10:22:51 +1000

    php

--- php/associativearray.php
+++ php/associativearray.php
@@ -1,10 +1,12 @@
 <?php
 function func() {
-    $a=array(
-        'data' => 1
-    );
-
+    $a=array();
+    $a['data'] = 1;
+    $a['foo'] = 2;
     return $a;
 }
 print func()['data'];
+print (string)in_array('data', array_keys(func()));
+print (string)in_array('foo', array_keys(func()));
+print var_export(func());
 ?>
