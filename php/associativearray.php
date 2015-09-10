34f088c317f9b61188f15e003938d8f3a0b1b134
Amit Saha <amitsaha.in@gmail.com>
2015-09-10 15:02:27 +1000

    php:associative array

--- php/associativearray.php
+++ php/associativearray.php
@@ -9,4 +9,9 @@ print func()['data'];
 print (string)in_array('data', array_keys(func()));
 print (string)in_array('foo', array_keys(func()));
 print var_export(func());
+
+$fhandler = fopen("dump", "w");
+fwrite($fhandler, (string) var_export(func(), true));
+fwrite($fhandler, (string) print_r(func(), true));
+fclose($fhandler);
 ?>
