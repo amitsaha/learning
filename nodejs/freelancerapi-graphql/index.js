9f8e7e1d32d3a337d4688509df1f5a43abe76cd8
Amit Saha <amitsaha.in@gmail.com>
2015-11-02 14:25:00 +1100

    graphql fln

--- nodejs/freelancerapi-graphql/index.js
+++ nodejs/freelancerapi-graphql/index.js
@@ -1,9 +1,7 @@
 var graphql = require('graphql');
 var graphqlHTTP = require('express-graphql');
 var express = require('express');
-//var https = require('https');
-//var requ = require('request');
-var rp = require('request-promise');
+var http_promise = require('request-promise');
 
 // Define project type
 var projectType = new graphql.GraphQLObjectType({
@@ -11,14 +9,15 @@ var projectType = new graphql.GraphQLObjectType({
     fields: {
         id: { type: graphql.GraphQLInt },
         owner_id: { type: graphql.GraphQLInt },
+        description: { type: graphql.GraphQLString},
     }
 });
 
 var get_request_data = {
-                        'headers': {'Freelancer-Developer-Auth-V1'
-                                    : '18;FLNCGQ5TPC1KFOTMSIFJME87N5NT2E63',
-                                   },
-                       }
+    'headers': {
+        'Freelancer-Developer-Auth-V1': process.env.TOKEN,
+    }
+}
 
 var schema = new graphql.GraphQLSchema({
     query: new graphql.GraphQLObjectType({
@@ -26,39 +25,33 @@ var schema = new graphql.GraphQLSchema({
         fields: {
             project: {
                 type: projectType,
-                // Query arguments
+                // Query project by ID
                 args: {
                     id: { type: graphql.GraphQLInt },
-                    owner_id: { type: graphql.GraphQLInt },
-                    description: {type: graphql.GraphQLString}
                 },
                 resolve: function (_, args) {
-                    var json_data;
                     if (args.id) {
-                        // Make a HTTP GET request to Freelancer.com API
                         get_request_data['uri'] = 'https://www.freelancer.com/api/projects/0.1/projects/' + args.id + '/';
-                        rp(get_request_data)
-                            .then(JSON.parse).
-                            then(function(json_data) {
-                                console.log(json_data['result']['owner_id']);
-                                return {'owner_id': json_data['result']['owner_id']}
-                            })
-                            .catch(function (err) {
-                                console.log(err);
+                        var get_result = http_promise(get_request_data)
+                            .then(function(data) {
+                                json_data = JSON.parse(data);
+                                return {'id': args.id,
+                                        'owner_id': json_data['result']['owner_id'],
+                                        'description': json_data['result']['preview_description'],
+                                       };
+                        })
+                            .catch(function(err) {
+                                return new Error(err['response']['body']);
                             });
-                    } else if (args.owner_id) {
-                        return {'owner_id': args.owner_id};
+                        return get_result;
                     } else {
                         return null;
                     }
-                 
                 }
             }
         }
-    }
-                                        )
+    })
 });
-
 console.log('GraphQL Server online!');
 express()
     .use('/graphql', graphqlHTTP({ schema: schema, pretty: true }))
