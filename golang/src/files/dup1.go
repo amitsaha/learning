fa52e142be00407b437712d108cc9a0945167ecb
Amit Saha <amitsaha.in@gmail.com>
2015-11-06 08:01:59 +1100

    Golang: dup1

---
+++ golang/src/files/dup1.go
@@ -0,0 +1,22 @@
+// Find lines which occur multiple times supplied via standard input
+
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
