46e4090b97ebcd2c2a503d214a5fe5f82d0e6abc
Amit Saha <amitsaha.in@gmail.com>
2015-11-07 13:05:35 +1100

    rust: chat fix

--- rust/chat/target/debug/deps/clock_ticks-7c95bf19b20ad8c3.d
+++ rust/chat/target/debug/deps/clock_ticks-7c95bf19b20ad8c3.d
@@ -1,4 +1,5 @@
-/Users/asaha/work/learning/rust/chat/target/debug/deps/libclock_ticks-7c95bf19b20ad8c3.rlib: /Users/asaha/.cargo/registry/src/github.com-0a35038f75765ae4/clock_ticks-0.0.5/src/lib.rs
+/Users/amit/work/learning/rust/chat/target/debug/deps/libclock_ticks-7c95bf19b20ad8c3.rlib: /Users/amit/.cargo/registry/src/github.com-0a35038f75765ae4/clock_ticks-0.0.5/src/lib.rs
 
-/Users/asaha/work/learning/rust/chat/target/debug/deps/clock_ticks-7c95bf19b20ad8c3.d: /Users/asaha/.cargo/registry/src/github.com-0a35038f75765ae4/clock_ticks-0.0.5/src/lib.rs
+/Users/amit/work/learning/rust/chat/target/debug/deps/clock_ticks-7c95bf19b20ad8c3.d: /Users/amit/.cargo/registry/src/github.com-0a35038f75765ae4/clock_ticks-0.0.5/src/lib.rs
 
+/Users/amit/.cargo/registry/src/github.com-0a35038f75765ae4/clock_ticks-0.0.5/src/lib.rs:
