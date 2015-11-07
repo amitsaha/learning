4ae1569d2a80b86eb67d828a1885a7065a862fce
Amit Saha <amitsaha.in@gmail.com>
2015-11-08 09:19:18 +1100

    Goroutines: demo of sync.WaitGroup

---
+++ golang/src/goroutines/demo1.go
@@ -0,0 +1,38 @@
+//http://www.goinggo.net/2014/01/concurrency-goroutines-and-gomaxprocs.html
+package main
+
+import (
+	"fmt"
+	"sync"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(2)
+
+	// Use to wait till the goroutines are finished executing
+	var wg sync.WaitGroup
+	wg.Add(2)
+	
+	fmt.Println("Starting Go Routines")
+	go func() {
+		defer wg.Done()
+
+		for char := 'a'; char < 'a'+26; char++ {
+			fmt.Printf("%c ", char)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		for number := 1; number < 27; number++ {
+			fmt.Printf("%d ", number)
+		}
+	}()
+
+	fmt.Println("Waiting To Finish")
+	wg.Wait()
+	
+	fmt.Println("\nTerminating Program")
+}
