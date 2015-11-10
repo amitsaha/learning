27a2500ec0b2df7b9d250a41d3a28ccc7d1ded78
Amit Saha <amitsaha.in@gmail.com>
2015-11-10 13:05:29 +1100

    Rust: chat

--- rust/chat/src/main.rs
+++ rust/chat/src/main.rs
@@ -1,5 +1,8 @@
 extern crate mio;
+
+use std::net::SocketAddr;
 use mio::*;
+use mio::tcp::*;
 
 struct WebSocketServer;
 
@@ -12,6 +15,12 @@ fn main() {
     let mut event_loop = EventLoop::new().unwrap();
     // Create a new instance of our handler struct:
     let mut handler = WebSocketServer;
+    let address = "0.0.0.0:11000".parse::<SocketAddr>().unwrap();
+    let server_socket = TcpListener::bind(&address).unwrap();
+    event_loop.register(&server_socket,
+                        Token(0),
+                        EventSet::readable(),
+                        PollOpt::edge()).unwrap();
     // ... and provide the event loop with a mutable reference to it:
     event_loop.run(&mut handler).unwrap();
 }
