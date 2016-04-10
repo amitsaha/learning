9895094c45668a8775eb6703842deb3a3e50f771
Amit Saha <amitsaha@users.noreply.github.com>
2016-04-10 18:13:07 +1000

    Update test1.go

--- libcontainer/test1.go
+++ libcontainer/test1.go
@@ -1,32 +1,168 @@
+package main
+
 import (
+	"github.com/opencontainers/runc/libcontainer"
 	"github.com/opencontainers/runc/libcontainer/configs"
+	"log"
 	"os"
+	"syscall"
 )
 
 func main() {
 	// The first argument is expected to be the path
 	// to the root FS directory
-	rootfs = os.Args[1]
+	rootfs := os.Args[1]
 	//defer remove(rootfs)
 
-	// Execute a long-running container
-	container1, err := newContainer(rootfs)
-	ok(t, err)
-	defer container1.Destroy()
-
-	stdinR1, stdinW1, err := os.Pipe()
-	ok(t, err)
-	init1 := &libcontainer.Process{
-		Cwd:   "/",
-		Args:  []string{"cat"},
-		Env:   standardEnvironment,
-		Stdin: stdinR1,
+	systemdFactory, err := libcontainer.New(rootfs, libcontainer.SystemdCgroups)
+	if err != nil {
+		log.Fatal(err)
 	}
-	err = container1.Start(init1)
-	stdinR1.Close()
-	defer stdinW1.Close()
-	ok(t, err)
 
-	// get the state of the first container
-	state1, err := container1.State()
+	//defer systemdFactory.Destroy()
+
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	config := &configs.Config{
+		Rootfs: rootfs,
+		Capabilities: []string{
+			"CAP_CHOWN",
+			"CAP_DAC_OVERRIDE",
+			"CAP_FSETID",
+			"CAP_FOWNER",
+			"CAP_MKNOD",
+			"CAP_NET_RAW",
+			"CAP_SETGID",
+			"CAP_SETUID",
+			"CAP_SETFCAP",
+			"CAP_SETPCAP",
+			"CAP_NET_BIND_SERVICE",
+			"CAP_SYS_CHROOT",
+			"CAP_KILL",
+			"CAP_AUDIT_WRITE",
+		},
+		Namespaces: configs.Namespaces([]configs.Namespace{
+			{Type: configs.NEWNS},
+			{Type: configs.NEWUTS},
+			{Type: configs.NEWIPC},
+			{Type: configs.NEWPID},
+			{Type: configs.NEWUSER},
+			{Type: configs.NEWNET},
+		}),
+		Cgroups: &configs.Cgroup{
+			Name:   "test-container",
+			Parent: "system",
+			Resources: &configs.Resources{
+				MemorySwappiness: nil,
+				AllowAllDevices:  false,
+				AllowedDevices:   configs.DefaultAllowedDevices,
+			},
+		},
+		MaskPaths: []string{
+			"/proc/kcore",
+		},
+		ReadonlyPaths: []string{
+			"/proc/sys", "/proc/sysrq-trigger", "/proc/irq", "/proc/bus",
+		},
+		Devices:  configs.DefaultAutoCreatedDevices,
+		Hostname: "testing",
+		Mounts: []*configs.Mount{
+			{
+				Source:      "proc",
+				Destination: "/proc",
+				Device:      "proc",
+				Flags:       defaultMountFlags,
+			},
+			{
+				Source:      "tmpfs",
+				Destination: "/dev",
+				Device:      "tmpfs",
+				Flags:       syscall.MS_NOSUID | syscall.MS_STRICTATIME,
+				Data:        "mode=755",
+			},
+			{
+				Source:      "devpts",
+				Destination: "/dev/pts",
+				Device:      "devpts",
+				Flags:       syscall.MS_NOSUID | syscall.MS_NOEXEC,
+				Data:        "newinstance,ptmxmode=0666,mode=0620,gid=5",
+			},
+			{
+				Device:      "tmpfs",
+				Source:      "shm",
+				Destination: "/dev/shm",
+				Data:        "mode=1777,size=65536k",
+				Flags:       defaultMountFlags,
+			},
+			{
+				Source:      "mqueue",
+				Destination: "/dev/mqueue",
+				Device:      "mqueue",
+				Flags:       defaultMountFlags,
+			},
+			{
+				Source:      "sysfs",
+				Destination: "/sys",
+				Device:      "sysfs",
+				Flags:       defaultMountFlags | syscall.MS_RDONLY,
+			},
+		},
+		UidMappings: []configs.IDMap{
+			{
+				ContainerID: 0,
+				HostID:      1000,
+				Size:        65536,
+			},
+		},
+		GidMappings: []configs.IDMap{
+			{
+				ContainerID: 0,
+				HostID:      1000,
+				Size:        65536,
+			},
+		},
+		Networks: []*configs.Network{
+			{
+				Type:    "loopback",
+				Address: "127.0.0.1/0",
+				Gateway: "localhost",
+			},
+		},
+		Rlimits: []configs.Rlimit{
+			{
+				Type: syscall.RLIMIT_NOFILE,
+				Hard: uint64(1025),
+				Soft: uint64(1025),
+			},
+		},
+	}
+
+	container, err := systemdFactory.Create("test-container", config)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	process := &libcontainer.Process{
+		Args:   []string{"ps"},
+		Env:    []string{"PATH=/bin"},
+		User:   "daemon",
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+
+	err = container.Start(process)
+	if err != nil {
+		log.Fatal(err)
+		container.Destroy()
+		return
+	}
+
+	// wait for the process to finish.
+	_, err = process.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// destroy the container.
+	container.Destroy()
 }
